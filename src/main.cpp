66611d096c6b18b149db8a1d02442a796e33a5f5
Jan Šilar <jansilar@post.cz>
2025-10-16 11:39:55 +0200

    Profiling, výpis do souboru.

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,15 @@
 #include "BeamSolver.h"
 #include <iostream>
 #include <iomanip>
-#include <algorithm> 
+#include <algorithm>
+#include <fstream>
+#include <string>
+#include <filesystem>
+#include <chrono>
 
 int main() {
-    int N = 500;          // počet uzlů
+    int N = 1000;          // počet uzlů
+    std::string fileName = "beam_results.csv";
     // Parametry nosníku
     double L = 2.0;           // délka [m]
     double E = 1.0e10;        // Young [Pa]
@@ -13,7 +18,14 @@ int main() {
     double q = 300.0;         // zatížení [N/m]
 
     BeamSolver solver(N, L, E, I, q);
+    // Start profiling
+    auto start = std::chrono::high_resolution_clock::now();
+    // Run solver
     solver.solve();
+    // End profiling
+    auto end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> elapsed = end - start;
+
 
     const auto& y = solver.getY();
     const auto& x = solver.getX();
@@ -27,4 +39,18 @@ int main() {
     double yAnal = solver.analyticMaxDeflection();
     std::cout << "\nAnalytic max deflection = " << yAnal*1000 << " mm\n";
     std::cout << "Numerical max deflection = " << (*std::max_element(y.begin(), y.end()))*1000 << " mm\n";
+
+    // Výstup do CSV
+    std::ofstream outFile(fileName);
+    outFile << "x[m],y[m]\n";
+    for (size_t i = 0; i < y.size(); ++i) {
+        outFile << std::fixed << std::setprecision(6)
+                << x[i] << "," << y[i] << "\n";
+    }
+    outFile.close();
+    std::cout << "\nResults written to " << std::filesystem::current_path() << "/" << fileName << " \n";
+    
+    std::cout << "Simulation time: " << elapsed.count() << " s\n";
+
+    
 }

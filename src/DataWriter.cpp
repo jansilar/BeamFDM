73bfbe6bfe9bf4f97f692eb7794381d883140ea7
Jan Šilar <jansilar@post.cz>
2025-10-17 10:53:43 +0200

    DataWriter

---
+++ src/DataWriter.cpp
@@ -0,0 +1,31 @@
+#include "DataWriter.h"
+
+DataWriter::DataWriter(const std::string& filename) {
+    outFile.open(filename);
+    if (!outFile.is_open()) {
+        throw std::runtime_error("Failed to open output file: " + filename);
+    }
+
+}
+
+DataWriter::~DataWriter() {
+    if (outFile.is_open()) {
+        outFile.close();
+    }
+}
+
+void DataWriter::writeHeader(const std::vector<double>& x) {
+    outFile << "Time[s], ";
+    for (size_t i = 0; i < x.size(); ++i) {
+        outFile << "x="<<x[i] << ", ";
+    }
+    outFile << "\n";
+}
+
+void DataWriter::writeStep(double t, const std::vector<double>& y) {
+    outFile << t << ", ";
+    for (size_t i = 0; i < y.size(); ++i) {
+        outFile << y[i] << ", ";
+    }
+    outFile << "\n";
+}
---
+++ src/DataWriter.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <fstream>
+#include <vector>
+#include <string>
+
+class DataWriter {
+public:
+    DataWriter(const std::string& filename);
+    ~DataWriter();
+    void writeHeader(const std::vector<double>& x);
+    void writeStep(double t, const std::vector<double>& y);
+private:
+    std::ofstream outFile;
+};
